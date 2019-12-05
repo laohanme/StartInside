0b666c830529e4f9540ea3f747e47c9cf89e3ff7
laohame <jimocheng97@hotmal.com>
2019-12-05 10:32:29 +0800

    first commit

---
+++ StartInside/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartInside
+//
+//  Created by laohanme on 05/12/2019.
+//  Copyright © 2019 laohanme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
